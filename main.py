f3d2bf72d83a8cdc7f060c2068cd15a6f21850c5
password <moto198720@yandex.ru>
2022-08-30 09:53:50 +0300

    5 ДЗ.

--- main.py
+++ main.py
@@ -1,215 +1,192 @@
 import psycopg2
 
-def create_db(conn):
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS clients(
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR(20) NOT NULL,
-        last_name VARCHAR(20) NOT NULL,
-        email VARCHAR(60) NOT NULL,
-        phones VARCHAR(60) 
-    );
-    """)
-def add_client(conn, first_name, last_name, email, phones=None):
-    cur.execute("""
-        INSERT INTO clients(first_name, last_name, email) VALUES(%s, %s, %s);
-        """, (first_name[i], last_name[i], email[i]))
-    cur.execute("""
-        SELECT last_name FROM clients;
-            """)
-    print(cur.fetchall())
-    cur.execute("""
-        SELECT * FROM clients;
-           """)
-    print('fetchall', cur.fetchall())
 
-def add_phone(conn, client_id, phone):
-    cur.execute("""
-         UPDATE clients SET phones=%s WHERE id=%s;
-         """, (phone, client_id))
-    cur.execute("""
-        SELECT * FROM clients;
-             """)
-    print(cur.fetchall())
-
-
-def change_client1(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    cur.execute("""
-         UPDATE clients SET first_name=%s WHERE id=%s;
-         """, (first_name, client_id))
-    cur.execute("""
-         SELECT * FROM clients;
-                """)
-    print(cur.fetchall())
-def change_client2(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    cur.execute("""
-          UPDATE clients SET last_name=%s WHERE id=%s;
-          """, (last_name, client_id))
-    cur.execute("""
-          SELECT * FROM clients;
-                 """)
-    print(cur.fetchall())
-def change_client3(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    cur.execute("""
-          UPDATE clients SET email=%s WHERE id=%s;
-          """, (email, client_id))
-    cur.execute("""
-          SELECT * FROM clients;
-                """)
-    print(cur.fetchall())
-def change_client4(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    cur.execute("""
-          UPDATE clients SET phones=%s WHERE id=%s;
-          """, (phones, client_id))
-    cur.execute("""
-          SELECT * FROM clients;
-                 """)
-    print(cur.fetchall())
-def delete_phone(conn, client_id, phones):
-    cur.execute("""
-              UPDATE clients SET phones=%s WHERE id=%s;
-              """, (phones, client_id))
-    cur.execute("""
-              SELECT * FROM clients;
-                     """)
-    print(cur.fetchall())
-
-def delete_client(conn, client_id):
-    cur.execute("""
-        DELETE FROM clients WHERE id=%s;
-             """, (client_id,))
-    cur.execute("""
-        SELECT * FROM clients;
-            """)
-    print(cur.fetchall())
-
-def find_client1(conn, first_name=None, last_name=None, email=None, phone=None):
-    cur.execute("""
-        SELECT * FROM clients WHERE first_name=%s;
-             """, (first_name,))
-    print(cur.fetchall())
-def find_client2(conn, first_name=None, last_name=None, email=None, phone=None):
-    cur.execute("""
-        SELECT * FROM clients WHERE last_name=%s;
-             """, (last_name,))
-    print(cur.fetchall())
-def find_client3(conn, first_name=None, last_name=None, email=None, phone=None):
-    cur.execute("""
-        SELECT * FROM clients WHERE email=%s;
-             """, (email,))
-    print(cur.fetchall())
-def find_client4(conn, first_name=None, last_name=None, email=None, phone=None):
-    cur.execute("""
-        SELECT * FROM clients WHERE phone=%s;
-             """, (phone,))
-    print(cur.fetchall())
-
-with psycopg2.connect(database="clients_db", user="postgres", password=" ") as conn:
+def get_id_data():
     with conn.cursor() as cur:
         cur.execute("""
-                DROP TABLE clients;
-                """)
-        create_db(conn)
-        first_name = []
-        last_name = []
-        email = []
-        for i in range(int(input("Введите количество клиентов: "))):
-            first_name.append(input("Введите имя клиента: "))
-            last_name.append(input("Введите фамилию клиента: "))
-            email.append(input("Введите email клиента: "))
-            add_client(conn, first_name=first_name, last_name=last_name, email=email, phones=None)
-        add_phone_question = input("Вы хотите  добавить номера телефонов клиентов? Введите: ДА/НЕТ ").upper()
-        if add_phone_question == "ДА":
-            for j in range(int(input("Введите количество клиентов, для которых Вы хотите указать номера телефонов: "))):
-                phone = []
-                client_id = int(input("Введите номер id клиента: "))
-                for i in range(int(input("Введите количество телефонных номеров для данного клиента: "))):
-                    phone.append(input("Введите номер телефона: "))
-                add_phone(conn, client_id=client_id, phone=phone)
-                print("Телефонные номера клиента успешно добавлены")
-        elif add_phone_question == "НЕТ":
-            print("Хорошо, номер телефона не будем указывать")
-        change_client_question = input("Вы хотите изменить данные клиента? Ввведите: ДА/НЕТ ").upper()
-        if change_client_question == "ДА":
-            change_client_question1 = input("Вы хотите изменить имя клиента? Введите: ДА/НЕТ ").upper()
-            if change_client_question1 == "ДА":
-                for j in range(int(input("Введите количество клиентов, у которых Вы хотите изменить имя: "))):
-                    client_id = int(input("Введите номер id клиента: "))
-                    first_name = input("Введите новое имя клиента: ")
-                    change_client1(conn, client_id, first_name=first_name)
-                    print("Изменения в имена клиентов внесены")
-            elif change_client_question1 == "НЕТ":
-                print("Хорошо, имена клиентов менять не будем")
-            change_client_question2 = input("Вы хотите изменить фамилию клиента? Введите: ДА/НЕТ ").upper()
-            if change_client_question2 == "ДА":
-                for j in range(int(input("Введите количество клиентов, у которых Вы хотите изменить фамилию: "))):
-                    client_id = int(input("Введите номер id клиента: "))
-                    last_name = input("Введите новую фамилию клиента: ")
-                    change_client2(conn, client_id, last_name=last_name)
-                    print("Изменения в фамилии клиентов внесены")
-            elif change_client_question2 == "НЕТ":
-                 print("Хорошо, фамилии клиентов менять не будем")
-            change_client_question3 = input("Вы хотите изменить email клиента? Введите: ДА/НЕТ ").upper()
-            if change_client_question3 == "ДА":
-                for j in range(int(input("Введите количество клиентов, у которых Вы хотите изменить email: "))):
-                    client_id = int(input("Введите номер id клиента: "))
-                    email = input("Введите новый email клиента: ")
-                    change_client3(conn, client_id, email=email)
-                    print("Изменения в email клиентов внесены")
-            elif change_client_question3 == "НЕТ":
-                print("Хорошо, email клиентов менять не будем")
-            change_client_question4 = input("Вы хотите изменить телефонные номера клиентов? Введите: ДА/НЕТ ").upper()
-            if change_client_question4 == "ДА":
-                for j in range(int(input("Введите количество клиентов, которым Вы хотите изменить телефонные номера: "))):
-                    phones = []
-                    client_id = int(input("Введите номер id клиента: "))
-                    for i in range(int(input("Введите количество телефонных номеров для данного клиента: "))):
-                        phones.append(input("Введите номер телефона: "))
-                    change_client4(conn, client_id, phones=phones)
-                print("Телефонные номера клиентов успешно обновлены")
-            elif change_client_question4 == "НЕТ":
-                print("Хорошо, номера телефонов клиентов менять не будем")
-            delete_phone_question = input("Вы хотите удалить номера телефонов клиента? Введите: ДА/НЕТ ").upper()
-            if delete_phone_question == "ДА":
-                for j in range(int(input("Введите количество клиентов, у которых Вы хотите удалить номера телефонов: "))):
-                    client_id = int(input("Введите номер id клиента: "))
-                    phones = 'Null'
-                    delete_phone(conn, client_id, phones=phones)
-                    print("Изменения в номера телефонов клиентов внесены")
-            elif delete_phone_question == "НЕТ":
-                print("Хорошо, номера телефонов клиентов удалять не будем")
-            delete_client_question = input("Вы хотите удалить клиента из базы? Введите: ДА/НЕТ ").upper()
-            if delete_client_question == "ДА":
-                for j in range(int(input("Введите количество клиентов, которых Вы хотите удалить из базы: "))):
-                    client_id = int(input("Введите номер id клиента, которого Вы хотите удалить: "))
-                    delete_client(conn, client_id)
-                    print("Изменения в базу клиентов внесены")
-            elif delete_client_question == "НЕТ":
-                print("Хорошо, клиентов удалять не будем")
-        elif change_client_question == "НЕТ":
-            print("Хорошо, данные клиентов менять не будем")
-        find_client_question = input("Вы хотите найти клиента в базе? Введите: ДА/НЕТ ").upper()
-        if find_client_question == "ДА":
-            for j in range(int(input("Введите количество клиентов, которых Вы хотите найти в базе: "))):
-                find_client_question1 = input("Как будем искать клиента? Введите: 'Имя'/'Фамилия'/'email'/'телефонный номер': ")
-                if find_client_question1 == "Имя":
-                    first_name = input("Введите имя клиента, которого Вы хотите найти: ")
-                    find_client1(conn, first_name=first_name)
-                    print("Данные по клиенту найдены")
-                elif find_client_question1 == "Фамилия":
-                    last_name = input("Введите фамилию клиента, которого Вы хотите найти: ")
-                    find_client2(conn, last_name=last_name)
-                    print("Данные по клиенту найдены")
-                elif find_client_question1 == "email":
-                    email = input("Введите email клиента, которого Вы хотите найти: ")
-                    find_client3(conn, email=email)
-                    print("Данные по клиенту найдены")
-                elif find_client_question1 == "телефонный номер":
-                    phone = input("Введите телефонный номер клиента, которого Вы хотите найти: ")
-                    find_client4(conn, phone=phone)
-                    print("Данные по клиенту найдены")
-        elif find_client_question == "НЕТ":
-            print("Хорошо, клиентов искать не будем")
-conn.close()
+            SELECT id FROM clients;
+        """)
+        list_id = cur.fetchall()
+        id_result = []
+        for id_number in list_id:
+            id_result.append(id_number[0])
+    return id_result
 
 
+def get_phones_data():
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT phone_number FROM phone_numbers;
+        """)
+        phones_list = cur.fetchall()
+        phones_result = []
+        for phone in phones_list:
+            phones_result.append(phone[0])
+    return phones_result
+
+
+def create_db():
+    with conn.cursor() as cur:
+        cur.execute("""
+                        CREATE TABLE IF NOT EXISTS clients(
+                        id SERIAL PRIMARY KEY,
+                        first_name VARCHAR(20) NOT NULL,
+                        last_name VARCHAR(20) NOT NULL,
+                        email VARCHAR(60) NOT NULL
+                        );
+                    """)
+
+        cur.execute("""
+                        CREATE TABLE IF NOT EXISTS phone_numbers(
+                        id SERIAL PRIMARY KEY,
+                        phone_number VARCHAR(11),
+                        client_id INTEGER NOT NULL REFERENCES clients(id) ON DELETE CASCADE
+                        );
+                    """)
+        conn.commit()
+
+
+def add_client(first_name, last_rname, email):
+    with conn.cursor() as cur:
+        cur.execute("""SELECT email FROM clients;""")
+        client_list = cur.fetchall()
+        client_result = []
+        for client in client_list:
+            client_result.append(client[0])
+
+        if email in client_result:
+            print(f'Данный {email} уже есть в базе данных')
+        else:
+            cur.execute("""INSERT INTO clients (first_name, last_name, email)
+                           VALUES (%s, %s, %s);
+                        """, (first_name, last_rname, email))
+        conn.commit()
+
+
+def add_phone(phone_number, client_id):
+    id_result = get_id_data()
+    phones_result = get_phones_data()
+    with conn.cursor() as cur:
+        if client_id in id_result:
+
+            if str(phone_number) in phones_result:
+                print(f'Данный номер телефона {phone_number} уже есть в базе данных')
+            else:
+                cur.execute("""
+                    INSERT INTO phone_numbers (phone_number, client_id)
+                    VALUES (%s, %s);
+                            """, (phone_number, client_id))
+            conn.commit()
+
+        else:
+            print(f'Клиент с номером id {client_id} отсутствует в базе данных')
+
+
+def change_phone(phone_number, client_id):
+    phones_res = get_phones_data()
+    with conn.cursor() as cur:
+        if str(phone_number) in phones_res:
+            print(f'Данный номер телефона - {phone_number} уже есть в базе данных')
+        else:
+            cur.execute("""
+                UPDATE phone_numbers SET phone_number = %s 
+                WHERE id = (
+                SELECT id from phone_numbers
+                WHERE client_id = %s 
+                LIMIT 1);
+                        """, (phone_number, client_id))
+        conn.commit()
+
+
+def change_client(client_id, first_name=None, last_name=None, email=None,
+                phone_number=None):
+    id_result = get_id_data()
+    with conn.cursor() as cur:
+        if client_id in id_result:
+            if first_name is not None:
+                cur.execute("""
+                    UPDATE clients SET first_name = %s 
+                    WHERE id = %s;
+                            """, (first_name, client_id))
+            if last_name is not None:
+                cur.execute("""
+                    UPDATE clients SET last_name = %s 
+                    WHERE id = %s;
+                            """, (last_name, client_id))
+            if email is not None:
+                cur.execute("""
+                    UPDTAE clients SET email = %s 
+                    WHERE id = %s;
+                            """, (email, client_id))
+            if phone_number is not None:
+                change_phone(phone_number, client_id)
+        else:
+            print(f'Клиент с номером id {client_id} отсутствует в базе данных')
+
+    conn.commit()
+
+
+def delete_phone(phone_number):
+    phones_result = get_phones_data()
+    with conn.cursor() as cur:
+        if str(phone_number) not in phones_result:
+            print(f'Данный номер телефона - {phone_number} не зарегистирован в базе данных')
+        else:
+            phone_number = str(phone_number)
+            cur.execute("""
+                DELETE FROM phone_numbers WHERE phone_number = %s;
+                        """, (phone_number,))
+        conn.commit()
+
+
+def delete_client(client_id):
+    id_result = get_id_data()
+    with conn.cursor() as cur:
+        if client_id in id_result:
+            cur.execute("""
+                DELETE FROM clients WHERE id = %s;
+                        """, (client_id,))
+        else:
+            print(f'Клиент с номером id {client_id} отсутствует в базе данных')
+
+
+def find_client(first_name, last_name, email, phone_number=None):
+    with conn.cursor() as cur:
+
+        if phone_number is not None:
+            phone_number = str(phone_number)
+            cur.execute("""
+                SELECT client_id FROM phone_numbers
+                WHERE phone_number = %s;
+                        """, (phone_number,))
+            id_number = cur.fetchall()[0][0]
+            print(f'Номер id клиента, с указанным Вами номером телефона: {id_number}')
+
+        else:
+            cur.execute("""
+                SELECT id FROM clients 
+                WHERE first_name = %s AND last_name = %s AND email = %s; 
+                        """, (first_name, last_name, email))
+            id_number = cur.fetchall()[0][0]
+            print(f'Номер id клиента с данными, которые Вы указали: {id_number}')
+
+
+if __name__ == "__main__":
+
+    with psycopg2.connect(database="clients_db", user="postgres", password="Motocikl") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                    DROP TABLE clients CASCADE;
+                     """)
+        create_db()
+        add_client('Денис', 'Сидоров', 'dude@mail.ru')
+        add_client('Евгений', 'Кочетов', 'evgen@mail.ru')
+        add_phone(79150010305, 1)
+        add_phone(79160020406, 2)
+        change_phone(79110030507, 1)
+        change_client(1, 'Александр', 'Дерюгин', phone_number=79160030608)
+        delete_client(2)
+        find_client('Александр', 'Дерюгин', 'dude@mail.ru')
+        find_client('Александр', 'Дерюгин', 'dude@mail.ru', 79160030608)
+
+    conn.close()
